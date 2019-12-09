73a5f163a674e5b6524903b5d5d746e7f7d5799d
boomgoat <talhaiqbal11@gmail.com>
2019-12-08 21:28:40 -0800

    changed logo, no side nav yet

---
+++ Scripts/navbar.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("scroll-nav");
+    document.getElementById("logo").src = "./Resources/Logo/Logo-Dark.png";
+  } else {
+    document.getElementById("navbar").classList.remove("scroll-nav");
+    document.getElementById("logo").src = "./Resources/Logo/Logo-Light.png";
+  }
+}
