82ce8e3eafc9edc72dd09a35fc518b084aa8acaf
boomgoat <talhaiqbal11@gmail.com>
2019-12-11 01:04:12 -0800

    fixed responsiveness issues

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,11 +1,17 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").classList.add("scroll-nav");
-    document.getElementById("logo").src = "./Resources/Logo/Logo-Dark.png";
-  } else {
-    document.getElementById("navbar").classList.remove("scroll-nav");
-    document.getElementById("logo").src = "./Resources/Logo/Logo-Light.png";
-  }
+    if (mq.matches) {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").classList.add("scroll-nav");
+            document.getElementById("logo").src = "./Resources/Logo/Logo-Dark.png";
+        } else {
+            document.getElementById("navbar").classList.remove("scroll-nav");
+            document.getElementById("logo").src = "./Resources/Logo/Logo-Light.png";
+        }
+    } else {
+        console.log("heliu")
+    }
 }
+
+const mq = window.matchMedia( "(min-width: 780px)" );
