c796fa6914ac0de303d22e382afd865d8f23b343
boomgoat <talhaiqbal11@gmail.com>
2019-12-12 00:14:22 -0800

    added menu overlay

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -17,8 +17,10 @@ function scrollFunction() {
 function toggle() {
     var hamburger = document.getElementById("nav-toggle");
     var menu = document.getElementById("side-bar");
+    var overlay = document.getElementById("menu-overlay");
     hamburger.classList.toggle("rotate");
     menu.classList.toggle("slide");
+    overlay.classList.toggle("overlay-fadeIn");
 }
 
 const mq = window.matchMedia( "(min-width: 780px)" );
