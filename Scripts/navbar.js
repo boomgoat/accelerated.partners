f4f65b67cb3cb18f27e64310f234f834dadea155
boomgoat <talhaiqbal11@gmail.com>
2019-12-13 22:12:37 -0800

    Completed Side-bar - Fully Responsive

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -4,9 +4,11 @@ function scrollFunction() {
     if (mq.matches) {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.getElementById("navbar").classList.add("scroll-nav");
+            document.getElementById("side-bar").classList.remove("side-block");
             document.getElementById("logo").src = "./Resources/Logo/Logo-Dark.png";
         } else {
             document.getElementById("navbar").classList.remove("scroll-nav");
+            document.getElementById("side-bar").classList.add("side-block");
             document.getElementById("logo").src = "./Resources/Logo/Logo-Light.png";
         }
     } else {
